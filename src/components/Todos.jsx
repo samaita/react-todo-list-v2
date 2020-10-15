54d6f73a0811d540c9c792be636cc641fef0c768
Samaita <gary.samaita@tokopedia.com>
2020-10-15 23:29:46 +0700

    Adding other components

---
+++ src/components/Todos.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Learn React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learn React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learn React</span>
+      </div>
+    </section>
+  );
+};
+
+export default Todos;
