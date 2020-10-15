712fd8b071f8ea23ce05a41d50485c622cca261a
Samaita <gary.samaita@tokopedia.com>
2020-10-16 00:05:33 +0700

    Move Todos data to pages level

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,19 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import Todo from "./Todo";
 
-const Todos = () => {
-  const todos = [
-    {
-      text: "Belajar di Rumah"
-    },
-    {
-      text: "Main di Rumah"
-    },
-    {
-      text: "Kerja di Rumah"
-    }
-  ];
-
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map((todo) => {
@@ -21,6 +11,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Paper from "../components/Paper";
 import Header from "../components/Header";
@@ -6,11 +6,23 @@ import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Belajar di Rumah"
+    },
+    {
+      text: "Main di Rumah"
+    },
+    {
+      text: "Kerja di Rumah"
+    }
+  ]);
+
   return (
     <Paper>
       <Header />
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </Paper>
   );
 };
